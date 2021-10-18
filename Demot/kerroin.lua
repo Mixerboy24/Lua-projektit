3be6b3bcea9c7ebe61874431ba830a29e4280661
Atte O <mixerboy24@gmail.com>
2021-10-18 15:42:21 +0300

    Testi lua koodi.

---
+++ Demot/kerroin.lua
@@ -0,0 +1,15 @@
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n-1)
+    end
+    
+end
+
+print("Tervetuloa kertoimeen")
+print("----")
+print("Aseta numero:")
+a = io.read("*number")
+print("----")
+print("tulos: ", fact(a))
